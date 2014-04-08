ab777652d2abc7d86db37dcd10afbaad4663ebd7
Krishn <Krishn2014@gmail.com>
2014-04-08 22:49:34 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sapp',
+      version='1.31',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Django==1.5.4'],
+     )
